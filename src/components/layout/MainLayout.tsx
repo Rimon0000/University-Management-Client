765152bf9c6e28cd4c4f054ff70f7b6a7b835d45
Your Name <rimonron.ad@gmail.com>
2024-02-28 12:41:55 +0600

    fixing the layout and setting basic sidebar option

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,20 +1,37 @@
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, MenuProps } from 'antd';
 import { createElement } from 'react';
 const { Header, Content, Footer, Sider } = Layout;
 
 
-const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-    (icon, index) => ({
-      key: String(index + 1),
-      icon: createElement(icon),
-      label: `nav ${index + 1}`,
-    }),
-  );
+const items : MenuProps['items'] = [
+    {
+        key: "1",
+        label: "Dashboard"
+    },
+    {
+        key: "2",
+        label: "Profile"
+    },
+    {
+        key: "3",
+        label: "User Management",
+        children: [
+            {
+                key: "11",
+                label: "Create Admin"
+            },
+            {
+                key: "12",
+                label: "Create Student"
+            }
+        ]
+    },
+]
 
 const MainLayout = () =>{
     return (
-        <Layout>
+        <Layout style={{height: '100vh'}}>
           <Sider
             breakpoint="lg"
             collapsedWidth="0"
@@ -25,7 +42,9 @@ const MainLayout = () =>{
               console.log(collapsed, type);
             }}
           >
-            <div className="demo-logo-vertical" />
+            <div style={{color: 'white', height: '4rem', display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <h1>PH University</h1>
+            </div>
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
           </Sider>
           <Layout>
