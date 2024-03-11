7c34dbbacff77d0b54499a85e694eac58e1da301
Your Name <rimonron.ad@gmail.com>
2024-03-12 00:03:33 +0600

    ignore serializable and non serializable object in redux actions

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from "./features/auth/authSlice"
 import { baseApi } from './api/baseApi'
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 
@@ -18,7 +18,11 @@ export const store = configureStore({
     [baseApi.reducerPath]: baseApi.reducer,
     auth: persistedAuthReducer
   },
-  middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(baseApi.middleware)
+  middleware: (getDefaultMiddlewares) => getDefaultMiddlewares({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }).concat(baseApi.middleware)
 })
 
 
--- test.js
+++ test.js
@@ -1,77 +1,89 @@
 
-export const AdminPath2 = [
-    {
-        name: "Dashboard",
-        path: "dashboard",
-        element: "AdminDashboard"
-    },
-    {
-        name: "User Management",
-        children: [
-            {
-                name: "Create Admin",
-                path: "create-student",
-                element: "<CreateAdmin></CreateAdmin>"
-            },
-            {
-                name: "Create Faculty",
-                path: "create-faculty",
-                element: "<CreateFaculty></CreateFaculty>"
-            },
-            {
-                name: "Create Student",
-                path: "create-student",
-                element: "<CreateStudent></CreateStudent>"
-            },
+// export const AdminPath2 = [
+//     {
+//         name: "Dashboard",
+//         path: "dashboard",
+//         element: "AdminDashboard"
+//     },
+//     {
+//         name: "User Management",
+//         children: [
+//             {
+//                 name: "Create Admin",
+//                 path: "create-student",
+//                 element: "<CreateAdmin></CreateAdmin>"
+//             },
+//             {
+//                 name: "Create Faculty",
+//                 path: "create-faculty",
+//                 element: "<CreateFaculty></CreateFaculty>"
+//             },
+//             {
+//                 name: "Create Student",
+//                 path: "create-student",
+//                 element: "<CreateStudent></CreateStudent>"
+//             },
 
-        ]
-    },
-]
-
-
-//for sidebar array
-const newArray = AdminPath2.reduce((acc, item) =>{
-    if(item.path && item.name){
-        acc.push({
-            key: item.name,
-            label: item.path,
-        })
-    }
-
-    if(item.children){
-        acc.push({
-            key: item.name,
-            label: item.name,
-            children: item.children.map(child =>({
-                key: child.name,
-                label: child.path
-            }))
-        })
-    }
-
-    return acc;
-}, [])
+//         ]
+//     },
+// ]
 
 
-//for routes
+// //for sidebar array
 // const newArray = AdminPath2.reduce((acc, item) =>{
-//     if(item.path && item.element){
+//     if(item.path && item.name){
 //         acc.push({
-//             path: item.path,
-//             element: item.element
+//             key: item.name,
+//             label: item.path,
 //         })
 //     }
 
 //     if(item.children){
-//         item.children.forEach(child =>{
-//             acc.push({
-//                 path: child.path,
-//                 element: child.element
-//             }) 
+//         acc.push({
+//             key: item.name,
+//             label: item.name,
+//             children: item.children.map(child =>({
+//                 key: child.name,
+//                 label: child.path
+//             }))
 //         })
 //     }
 
 //     return acc;
 // }, [])
 
-console.log(newArray);
+
+// //for routes
+// // const newArray = AdminPath2.reduce((acc, item) =>{
+// //     if(item.path && item.element){
+// //         acc.push({
+// //             path: item.path,
+// //             element: item.element
+// //         })
+// //     }
+
+// //     if(item.children){
+// //         item.children.forEach(child =>{
+// //             acc.push({
+// //                 path: child.path,
+// //                 element: child.element
+// //             }) 
+// //         })
+// //     }
+
+// //     return acc;
+// // }, [])
+
+// console.log(newArray);
+
+
+const obj = {
+    name: "rimon",
+    role: "student",
+    age: 26,
+    greet: () => {
+        return "Hello World"
+    }
+}
+
+console.log(JSON.stringify(obj));
