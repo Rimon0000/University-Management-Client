c8ad351a2227e28272bd280362210f0f3846451c
Your Name <rimonron.ad@gmail.com>
2024-03-13 01:32:58 +0600

    login redirect and logout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,14 @@
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { TUser, setUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () =>{
+    const navigate = useNavigate()
     const dispatch = useAppDispatch()
 
     const {register, handleSubmit} = useForm({
@@ -15,17 +18,27 @@ const Login = () =>{
         }
     })
 
-    const [login, { error}] = useLoginMutation()
+    const [login] = useLoginMutation()
 
-    const onSubmit = async(data) =>{
-        const loginInfo = {
-            id: data.id,
-            password: data.password
+    const onSubmit = async(data: FieldValues) =>{
+        const toastId = toast.loading('Logging in');
+
+        try {
+            const loginInfo = {
+                id: data.id,
+                password: data.password
+            }
+    
+            const res = await login(loginInfo).unwrap()
+            const user = verifyToken(res.data.accessToken) as TUser;
+            dispatch(setUser({user: user, token: res.data.accessToken}))
+            toast.success('Logged in.', {id: toastId, duration: 2000})
+            navigate(`/${user.role}/dashboard`)
+
+        } catch (error) {
+            toast.error('Something went wrong!', {id: toastId, duration: 2000})
         }
-
-        const res = await login(loginInfo).unwrap()
-        const user = verifyToken(res.data.accessToken)
-        dispatch(setUser({user: user, token: res.data.accessToken}))
+        
     }
 
     return (
