3aa84262c735fd5f252031cdb87d47d8e674d28e
Your Name <rimonron.ad@gmail.com>
2024-04-04 02:24:45 +0600

    Setting up the next step for user management

---
+++ src/pages/admin/academicManagement/CreateAcademicDepartment.tsx
@@ -0,0 +1,11 @@
+
+const CreateAcademicDepartment = () =>{
+
+    return (
+        <div>
+            <h1>This is CreateAcademicDepartment</h1>
+        </div>
+    )
+}
+
+export default CreateAcademicDepartment;
--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -34,7 +34,6 @@ export const authSlice = createSlice({
             state.token = null
         }
     }
-
 })
 
 
