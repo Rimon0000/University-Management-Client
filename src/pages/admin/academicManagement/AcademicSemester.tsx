a5a3ac5f7f519c111b06323bd2ae8446c1930e33
Your Name <rimonron.ad@gmail.com>
2024-04-30 20:53:12 +0600

    Show semester data using antDesign table

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,12 +1,78 @@
+import { Table, TableColumnsType, TableProps } from "antd";
 import { useGetAcademicSemestersQuery } from "../../../redux/features/admin/academicManagement";
 
+type DataType = {
+    key: React.Key;
+    name: string;
+    age: number;
+    address: string;
+}
+
+
 const AcademicSemester = () =>{
-    const {data} = useGetAcademicSemestersQuery(undefined)
-    console.log(data);
+    const {data: semesterData} = useGetAcademicSemestersQuery(undefined)
+    console.log(semesterData);
+
+    const tableData = semesterData?.data?.map(({_id, name, year, startMonth, endMonth}) => ({
+        _id,
+        name,
+        year,
+        startMonth,
+        endMonth
+    }))
+
+
+    const columns: TableColumnsType<DataType> = [
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          showSorterTooltip: { target: 'full-header' },
+          filters: [
+            {
+              text: 'Joe',
+              value: 'Joe',
+            },
+            {
+              text: 'Jim',
+              value: 'Jim',
+            },
+            {
+              text: 'Submenu',
+              value: 'Submenu',
+              children: [
+                {
+                  text: 'Green',
+                  value: 'Green',
+                },
+                {
+                  text: 'Black',
+                  value: 'Black',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          title: 'Year',
+          dataIndex: 'year',
+        },
+        {
+          title: 'Start Month',
+          dataIndex: 'startMonth',
+        },
+        {
+            title: 'End Month',
+            dataIndex: 'endMonth',
+          },
+      ];
+      
+
+      const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+        console.log(filters);
+      };
+
     return (
-        <div>
-            <h1>This is AcademicSemester</h1>
-        </div>
+        <Table columns={columns} dataSource={tableData} onChange={onChange} showSorterTooltip={{ target: 'sorter-icon' }}/>
     )
 }
 
